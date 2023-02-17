8c5bf07611975f519024107d020aef4d60cf8afb
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-17 18:07:23 -0300

    feat: add loading functionality when adding user, also made the display of the skeleton loader better

--- client/src/components/UsersList.jsx
+++ client/src/components/UsersList.jsx
@@ -3,28 +3,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
+import { faker } from "@faker-js/faker";
+import { useThunk } from "../hooks/useThunk";
+import axios from "axios";
 
 export function UsersList() {
-  const dispatch = useDispatch();
-  const { data, isLoading, error } = useSelector((state) => state.users);
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, []);
+  const { data } = useSelector((state) => state.users);
+  const [doFetchUsers, isLoadingUser, isLoadingUserError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, isCreatingUserError] = useThunk(addUser);
 
-  const handleUserAdd = () => {
-    dispatch(addUser());
+  useEffect(() => {
+    try {
+      doFetchUsers();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [doFetchUsers]);
+
+  const handleUserAdd = async () => {
+    try {
+      doCreateUser();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const renderedUsers = data.map((user) => (
-    <div className="w-full rounded border mb-3 p-3" key={user.id}>
-      {user.name}
+  const renderedUsers = data.map((user, i) => (
+    <div className="w-full rounded border mb-3 p-3" key={i}>
+      {user.username}
     </div>
   ));
 
-  if (isLoading) {
-    return <Skeleton times={6} className="w-full h-10" />;
+  if (isCreatingUser) {
+    console.log("creatinh user");
   }
-  if (error) {
+  if (isLoadingUserError) {
     return <div>Error fetching users...</div>;
   }
 
@@ -32,9 +48,22 @@ export function UsersList() {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h2 className="m2 text-xl">Users</h2>
-        <Button onClick={handleUserAdd}>+ Add new user</Button>
+        <Button
+          className="w-15 h-10"
+          loading={isCreatingUser}
+          onClick={handleUserAdd}
+        >
+          + Add new user
+        </Button>
       </div>
-      {renderedUsers}
+      {isLoadingUser ? (
+        <Skeleton times={10} className="w-full h-10" />
+      ) : data.length > 0 ? (
+        renderedUsers
+      ) : (
+        "0 Users"
+      )}
+      {}
     </div>
   );
 }
