d2b2d9c4823082dc961993e356bb28029fb9e3aa
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-17 18:11:02 -0300

    bugfix: fix all bugs related to the integration between front end and database

--- server/server.js
+++ server/server.js
@@ -8,16 +8,33 @@ const pool = require("./db");
 
 const cors = require("cors");
 
+const { v4: uuidv4, v4 } = require("uuid");
+
 app.use(cors({ origin: "http://127.0.0.1:5173" }));
+app.use(express.json());
 
 //Get all todos from our postgresql db
 app.get("/users", async (req, res) => {
   try {
-    const response = await pool.query("SELECT * FROM app_user");
+    const response = await pool.query("SELECT * FROM app_users");
     res.json(response.rows);
   } catch (error) {
     console.error(error);
   }
+});
+
+app.post("/users/post", async (req, res) => {
+  const { username, title, date } = req.body;
+  console.log(username, title, date);
+  const id = uuidv4();
+  console.log(id, username, title, date);
+
+  const response = await pool.query(
+    "INSERT INTO app_users VALUES ($1, $2, $3, $4)",
+    [id, username, title, date]
+  );
+
+  res.json({ id, username, title, date });
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
