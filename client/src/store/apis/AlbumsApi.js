f8767c318ae357671d862fb39e58d699fd26d0c8
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-24 12:53:05 -0300

    bugfix: solve the bug of multiple requests when creating a new album + add dev only function to add a pause when making requests

--- client/src/store/apis/AlbumsApi.js
+++ client/src/store/apis/AlbumsApi.js
@@ -1,17 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+//DEV ONLY!--REMOVE FOR PRODUCTION
+const pause = (duration) =>
+  new Promise((resolve) => setTimeout(resolve, duration));
+
 const albumsApi = createApi({
   reducerPath: "albums",
 
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:8000",
+
+    //DEV ONLY!--REMOVE FOR PRODUCTION
+    fetchFn: async (...args) => {
+      await pause(3000);
+      return fetch(...args);
+    },
   }),
 
   endpoints(builder) {
     return {
       fetchAlbums: builder.query({
         //Adding a tag to make the request be reexecuted once we make a mutation to the database
-        providesTags: ["Album"],
+        providesTags: (result, error, user) => [{ type: "Album", id: user }],
         query: (userId) => {
           return {
             url: `/albums`,
@@ -25,7 +35,9 @@ const albumsApi = createApi({
 
       addAlbum: builder.mutation({
         //Whenever we run this mutation, we are going to look at all the queries that uses this tag and invalidate it to refetch the data
-        invalidatesTags: ["Album"],
+        invalidatesTags: (result, error, album) => [
+          { type: "Album", id: album.userId },
+        ],
         query: (album) => {
           return {
             url: `/albums`,
