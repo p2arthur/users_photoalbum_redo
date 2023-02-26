659e69671ed89b01ca5b2b4148c2013c4094ea2c
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-26 00:15:53 -0300

    store: add photosApi reducers into the store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,10 +2,18 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { albumsApi } from "./apis/AlbumsApi";
+import { photosApi } from "./apis/PhotosApi";
 export const store = configureStore({
-  reducer: { users: usersReducer, [albumsApi.reducerPath]: albumsApi.reducer },
+  reducer: {
+    users: usersReducer,
+    [photosApi.reducerPath]: photosApi.reducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(albumsApi.middleware);
+    return getDefaultMiddleware().concat(
+      albumsApi.middleware,
+      photosApi.middleware
+    );
   },
 });
 
@@ -19,3 +27,5 @@ export {
   useAddAlbumMutation,
   useRemoveAlbumMutation,
 } from "./apis/AlbumsApi";
+
+export { useFetchPhotosQuery } from "./apis/PhotosApi";
